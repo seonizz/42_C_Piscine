57f30358b7d7045b040814d683845a479350f171
Yoonsun Hwang <yoohwang@c1r9s2.42gyeongsan.kr>
2026-08-11 22:46:01 +0900

    재귀함수ver

--- C&Shell/C04/ex04/ft_putnbr_base.c
+++ C&Shell/C04/ex04/ft_putnbr_base.c
@@ -6,7 +6,7 @@
 /*   By: yoohwang <yoohwang@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/11 22:19:09 by yoohwang          #+#    #+#             */
-/*   Updated: 2026/08/11 22:19:19 by yoohwang         ###   ########.fr       */
+/*   Updated: 2026/08/11 22:42:00 by yoohwang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,39 +38,22 @@ int	ft_base_check(char *base)
 
 void	ft_putnbr_base(int nbr, char *base)
 {
-	int		idx;
 	int		len;
 	long	n;
-	char	stack[50];
 
-	len = 0;
-	idx = -1;
 	len = ft_base_check(base);
 	if (len == 0)
 		return ;
 	n = nbr;
-	if (n == 0)
-	{
-		write(1, &base[0], 1);
-		return ;
-	}
 	if (n < 0)
 	{
 		write(1, "-", 1);
 		n = -n;
 	}
-	while (n)
-	{
-		stack[++idx] = base[n % len];
-		n = n / len;
-	}
-	while (idx != -1)
-	{
-		write(1, &stack[idx], 1);
-		idx--;
-	}
+	if (n >= len)
+		ft_putnbr_base(n / len, base);
+	write(1, &base[n % len], 1);
 }
-
 /**
  * @brief 정수 nbr을 base 에 채워진 문자열대로 처리
  * while (nbr) 동안 -라면 음수 처리, 

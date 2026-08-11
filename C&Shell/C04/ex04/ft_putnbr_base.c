689bece4e9ba715be99223b437a30d1b9fad8f13
Yoonsun Hwang <yoohwang@c1r9s2.42gyeongsan.kr>
2026-08-11 22:21:30 +0900

    NEW

--- C&Shell/C04/ex04/ft_putnbr_base.c
+++ C&Shell/C04/ex04/ft_putnbr_base.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: yoohwang <yoohwang@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/08/10 12:17:39 by yoohwang          #+#    #+#             */
-/*   Updated: 2026/08/10 21:27:01 by yoohwang         ###   ########.fr       */
+/*   Created: 2026/08/11 22:19:09 by yoohwang          #+#    #+#             */
+/*   Updated: 2026/08/11 22:19:19 by yoohwang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,11 @@ int	ft_base_check(char *base)
 	int	j;
 
 	i = 0;
+	if (base[0] == '\0' || base[1] == '\0')
+		return (0);
 	while (base[i])
 	{
-		if (base[i] == '-' || base[i] == '+')
+		if (base[i] == '+' || base[i] == '-')
 			return (0);
 		j = i + 1;
 		while (base[j])
@@ -36,23 +38,31 @@ int	ft_base_check(char *base)
 
 void	ft_putnbr_base(int nbr, char *base)
 {
-	int		len;
 	int		idx;
+	int		len;
+	long	n;
 	char	stack[50];
 
+	len = 0;
 	idx = -1;
 	len = ft_base_check(base);
-	if (len)
+	if (len == 0)
 		return ;
-	if (nbr < 0)
+	n = nbr;
+	if (n == 0)
+	{
+		write(1, &base[0], 1);
+		return ;
+	}
+	if (n < 0)
 	{
 		write(1, "-", 1);
-		nbr = -nbr;
+		n = -n;
 	}
-	while (nbr)
+	while (n)
 	{
-		stack[++idx] = base[nbr % len];
-		nbr /= len;
+		stack[++idx] = base[n % len];
+		n = n / len;
 	}
 	while (idx != -1)
 	{
@@ -62,14 +72,6 @@ void	ft_putnbr_base(int nbr, char *base)
 }
 
 /**
- * @brief 주어진 숫자를 주어진 진법으로 표현해서 출력하는 함수
- * 
- * 0. nbr이 음수인지 양수인지 쳌
- * 1. ft_check_base()함수를 통해 진법을 체크하고 맞는 진법이라면 return len(base)
- * 2. 주어진 숫자를 진법으로 바꾼 후 스택에 저장한다.
- * 3. 스택의 idx를 하나씩 줄여가면서 스택에 있는 요소를 wrtie()한다.
- * 
- * @param int nbr: 진법으로 바꿀 숫자
- * @param char *base: 주어진 진법
- * @return 표현된 진법을 출력
+ * @brief 정수 nbr을 base 에 채워진 문자열대로 처리
+ * while (nbr) 동안 -라면 음수 처리, 
  */
